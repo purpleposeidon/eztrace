fe6597f7f82625dbdfc62b5e5d4b6d6996e0748b
neptunepink <purpleposeidon@gmail.com>
2021-08-07 01:06:12 -0700

    Add trace!("label"), trace!(#…) for {:#?}.

--- eztrace.rs
+++ eztrace.rs
@@ -46,12 +46,53 @@
 /// trace!();
 /// // eztrace.rs:1
 /// ```
+///
+/// To print a label:
+///
+/// ```
+/// # #[macro_use] extern crate eztrace;
+/// trace!("searching haystack for needles");
+/// // searching haystack for needles
+/// ```
+///
+/// You can also prefix with a `#` to get `{:#?}`-style format codes (tho you might just use
+/// `dbg!()` instead…)
+///
+/// ```
+/// # #[macro_use] extern crate eztrace;
+/// trace!(#);
+/// // eztrace.rs:1
+/// trace!(#"hello");
+/// // "hello"
+/// #[derive(Debug, Default)]
+/// struct Coords {
+///     x: f32,
+///     y: f32,
+/// }
+/// let zero = Coords::default();
+/// trace!(#zero);
+/// // zero: Coords {
+/// //     x: 0.0,
+/// //     y: 0.0,
+/// // }
+/// ```
 #[macro_export]
 macro_rules! trace {
-    () => {
-        println!(trace!(@line));
+    () => { println!(trace!(@line)); };
+    (#) => { println!(trace!(@line)); };
+    (#$label:literal) => {
+        println!("{:?}", $label);
     };
-    ($($IT:expr),* $(,)*) => {
+    ($label:literal) => {
+        println!("{}", $label);
+    };
+    (#$($IT:expr),* $(,)?) => {
+        println!(
+            trace!(@#fmt $($IT),*),
+            $(&$IT),*
+        );
+    };
+    ($($IT:expr),* $(,)?) => {
         println!(
             trace!(@fmt $($IT),*),
             $(&$IT),*
@@ -62,12 +103,22 @@ macro_rules! trace {
             file!(), ":", line!(),
         )
     };
+    (@#fmt $($IT:expr),*) => {
+        concat!(
+            trace!(@stringify $($IT,)*),
+            ":",
+            $(trace!(@#fmtcode $IT)),*
+        )
+    };
     (@fmt $($IT:expr),*) => {
         concat!(
             trace!(@stringify $($IT,)*),
             ":",
             $(trace!(@fmtcode $IT)),*
         )
+    };
+    (@#fmtcode $_:expr) => {
+        " {:#?}"
     };
     (@fmtcode $_:expr) => {
         " {:?}"
@@ -119,7 +170,14 @@ mod tests {
 
     #[test]
     fn single() {
-        trace!("hello");
+        let hello = "hello";
+        trace!(hello);
+    }
+
+    #[test]
+    fn literal() {
+        trace!("hi");
+        trace!(0xED);
     }
 
     #[test]
@@ -137,5 +195,23 @@ mod tests {
         trace!(a * a + b * b, c * c);
         // a * a + b * b, c * c: 25 25
         trace!();
+
+
+        #[derive(Debug, Default)]
+        struct Coords {
+            x: f32,
+            y: f32,
+        }
+        trace!(#);
+        // eztrace.rs:7
+        trace!(#"hello");
+        // "hello"
+        let zero = Coords::default();
+        trace!(#zero);
+        // zero: Coords {
+        //     x: 0.0,
+        //     y: 0.0,
+        // }
+
     }
 }
